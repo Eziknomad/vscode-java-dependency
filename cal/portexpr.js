a2d591f795931d06c1801eeab38dee454b87614e
ZekNmad <89168388+Eziknomad@users.noreply.github.com>
2024-08-02 00:16:14 -0400

    changes done to cel App

---
+++ cal/portexpr.js
@@ -0,0 +1,4 @@
+// Express server on port 3000
+// http://localhost:3000    ->  Hello World from Express    ->  GET request to root path    ->  200 OK  ->  text/html   ->  Hello World from Express    ->  0.000 ms
+// Return the current time
+// http://localhost:3000/time    ->  The time is 12:34:56 PM    ->  GET request to /time path    ->  200 OK  ->  text/html   ->  The time is 12:34:56 PM    ->  0.000 ms
